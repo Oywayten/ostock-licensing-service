df59c44b0fe1230cf55669286e47471faa2a9d19
Oywayten <oywayten@gmail.com>
2024-01-27 22:21:41 +0300

    add LicenseController, LicenseService and rename OstockLicensingServiceApplication to LicenseServiceApplication

--- src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OstockLicensingServiceApplication {
+public class LicenseServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OstockLicensingServiceApplication.class, args);
+        SpringApplication.run(LicenseServiceApplication.class, args);
     }
 
 }
