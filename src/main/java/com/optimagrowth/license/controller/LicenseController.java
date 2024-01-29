9e1a842ac1b01580007c40efce28f0a7e787b982
Oywayten <oywayten@gmail.com>
2024-01-30 01:31:37 +0300

    add HATEOAS

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 /**
  * Oywayten 27.01.2024.
  */
@@ -20,9 +23,22 @@ public class LicenseController {
     private final LicenseService licenseService;
 
     @GetMapping("/{licenseId}")
-    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
-                                              @PathVariable("licenseId") String licenseId) {
+    public ResponseEntity<License> getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
         License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, license.getLicenseId()))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license, null))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId(), null))
+                        .withRel("deleteLicense"));
         return ResponseEntity.ok(license);
     }
 
