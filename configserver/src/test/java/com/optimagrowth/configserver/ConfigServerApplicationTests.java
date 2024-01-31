7c71e70f7dd94d8ffa6ba3606fa65ab37e62ede6
Oywayten <oywayten@gmail.com>
2024-02-01 00:23:15 +0300

    add configs for licensing-service

--- configserver/src/test/java/com/optimagrowth/configserver/ConfigServerApplicationTests.java
+++ configserver/src/test/java/com/optimagrowth/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.configserver;
+package com.optimagrowth.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigserverApplicationTests {
+class ConfigServerApplicationTests {
 
     @Test
     void contextLoads() {
